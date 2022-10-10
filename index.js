673c829e255c7d343bd8907585067e316cb7aef1
alexpxrad <alexpxrad@gmail.com>
2022-10-10 18:01:28 -0400

    adding functionalities to game

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container")
 
 let playerLeftSpace = 50
-let playerBottomSpace = 150
+let playerBottomSpace = 300
 
 const player = document.createElement("div")
 
@@ -11,7 +11,7 @@ function createPlayer() {
     player.classList.add("player")
 
     player.style.bottom = playerBottomSpace + "px"
-    player.style.left = playerLeftSpace + "px"
+    player.style.left = ground[0].left
 
      container.appendChild(player)
     console.log("aa")
@@ -20,8 +20,81 @@ function createPlayer() {
 class Ground {
   constructor(newGroundBottom) {
     this.bottom = newGroundBottom
-    this.left = Math.random() * 425
+    this.left = Math.random() * 325
+    this.visual = document.createElement("div")
+
+    const visual = this.visual
+    visual.classList.add("ground")
+    visual.style.left = this.left + "px"
+    visual.style.bottom = this.bottom + "px"
+    container.appendChild(visual)
   }
 }
 
 createPlayer()
+
+let groundCount = 5
+let grounds = []
+
+function createGround() {
+        for(let i = 0; i < groundCount; i++) {
+                let groundGap = 700 / groundCount
+                let groundBottom = 100 + i * groundGap
+                let ground = new Ground(groundBottom)
+                grounds.push(ground)
+        }
+}
+
+
+function moveGrounds() {
+        if(playerLeftSpace > 200) {
+             grounds.forEach(ground => {
+                ground.bottom -= 5 
+                let visual = ground.visual
+                visual.style.bottom = ground.bottom + 'px'
+             })
+        }
+}
+
+let upTimerId
+let downTimerId
+
+
+function jump() {
+    clearInterval(downTimerId)
+    isJumping = true
+    upTimerId = setInterval(() => {
+            playerBottomSpace += 20;
+            player.style.bottom = playerBottomSpace + "px"
+    }, 30)
+}
+
+function fall() {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(() => {
+            playerBottomSpace -= 5;
+            player.style.bottom = playerBottomSpace + "px"    
+        }, 30);
+        if(playerBottomSpace <= 0) {
+            endTheGame()
+        }
+
+}
+
+
+let isGameover = false 
+function startGame() {
+        if(!isGameover) {
+            createPlayer()
+            createGround()
+            setInterval(moveGrounds(), 30 )
+        }
+}
+
+function endTheGame() {
+    isGameover = true 
+    clearInterval(upTimerId)
+    clearInterval(downTimerId)
+}
+
+startGame()
